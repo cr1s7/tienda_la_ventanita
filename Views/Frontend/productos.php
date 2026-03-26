2c08fcf5c29364e9b0951b2da5a56fa93e85dc8f
cr1s7 <scristianoricardo2@gmail.com>
2026-03-26 12:55:06 -0500

    Delete Views directory

--- Views/Frontend/productos.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($productos as $p): ?>
-    <div>
-        <h5><?= $p['nombre'] ?></h5>
-        <p>Precio: $<?= number_format($p['preUnitario']) ?></p>
-        <p>Stock: <?= $p['stock'] ?></p>
-    </div>
-<?php endforeach; ?>
